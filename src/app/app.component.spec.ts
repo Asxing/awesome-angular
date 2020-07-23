df8ccb6662a87a1aa0038e9d992dce5c1e23a7d2
holddie <holddie@163.com>
2020-07-23 17:47:53 +0800

    feature: 做了一些单元测试相关的注释

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {browser, logging} from 'protractor';
 
+// 测试入口，参数为测试名和方法
 describe('AppComponent', () => {
+  // 每个测试定义的 SetUp 方法
   beforeEach(async(() => {
+    console.log('start app test');
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -10,6 +14,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  // 测试用例
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -19,6 +24,7 @@ describe('AppComponent', () => {
   it(`should have as title 'awesome-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // 断言使用
     expect(app.title).toEqual('awesome-angular');
   });
 
@@ -26,6 +32,13 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+    // 使用 QuerySelector 获取页面元素
     expect(compiled.querySelector('.content span').textContent).toContain('awesome-angular app is running!');
   });
+
+  // 每个测试的 TearDown
+  afterEach(async () => {
+    console.log('end app test');
+  });
+
 });
